fc4b09e0e653532c086e2e8e5be4fa40ad35afa1
Wouter Deconinck <wdconinc@gmail.com>
2020-08-22 14:01:35 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,21 @@
+[Kassiopeia](https://github.com/KATRIN-Experiment/Kassiopeia) simulates electric and magnetic fields and particle tracking.
+
+# Synopsis
+
+```
+cmake_Kassiopeia.sh <version>
+```
+
+# Description
+
+This script downloads and builds Kassiopeia from the source distribution. It installs the resulting binaries in `/usr/local/Kassiopeia/<version>`. Multiple versions can be installed next to each other, and the desired version can be loaded by sourcing the file `bin/kasperenv.sh`.
+
+# Examples
+
+```
+./cmake_Kassiopeia.sh 4.10.02.p02
+```
+
+# License
+
+This work is provided under the license described in the `LICENSE` file.
---
+++ cmake_Kassiopeia.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+version=$1
+origdir=`dirname $0`
+
+# Set compiler variables (add version, e.g. "-5", "-6", to override)
+export CC=gcc
+export CXX=g++
+
+if test -z "${version##*.*}" ; then
+  echo "Downloading Kassiopeia v${version}.tar.gz..."
+  until test -f v${version}.tar.gz
+  do
+    wget https://github.com/KATRIN-Experiment/Kassiopeia/archive/v${version}.tar.gz
+  done
+
+  echo "Unpacking Kassiopeia v${version}.tar.gz..."
+  until test -d Kassiopeia-${version}
+  do
+    tar -zxvf v${version}.tar.gz
+  done
+else
+  echo "Cloning Kassiopeia ${version}..."
+  until test -d Kassiopeia-${version}
+  do
+    git clone -b ${version} https://github.com/KATRIN-Experiment/Kassiopeia.git Kassiopeia-${version}
+  done
+
+  echo "Pulling Kassiopeia ${version}..."
+  git -C Kassiopeia-${version} pull
+fi
+
+mkdir -p Kassiopeia-${version}-build
+cd Kassiopeia-${version}-build
+
+echo "Configuring Kassiopeia ${version}..."
+cmake -DCMAKE_BUILD_TYPE=Debug \
+ -DCMAKE_INSTALL_PREFIX=/usr/local/Kassiopeia/${version} \
+ -DKassiopeia_ENABLE_DEBUG=ON \
+ -DKommon_ENABLE_DEBUG=ON \
+ -DKGeoBag_ENABLE_DEBUG=ON \
+ -DKASPER_USE_VTK=ON \
+ -DKASPER_USE_GSL=ON \
+ -DKASPER_USE_TBB=ON \
+ -DKEMField_USE_MPI=OFF \
+ -DKEMField_USE_OPENCL=OFF \
+ -DKEMField_USE_ZLIB=ON \
+ ../Kassiopeia-${version}
+
+j=4
+#j=$(nproc)
+echo "Make will use $j parallel jobs."
+
+echo "Building Kassiopeia ${version}..."
+make -j $j -k
+
+echo "Installing Kassiopeia ${version}..."
+make -j $j install
+
+cd "${origdir}"
+
